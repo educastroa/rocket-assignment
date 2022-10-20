b95af4de683bee00b43b45a3a116efbc8c918353
Eduardo Castro <eduardocastro.a@hotmail.com>
2022-10-20 14:37:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+  const [results, setResults] = useState([])
+  const [input, setInput] = useState()
+
+
+  const fetchData = (parameter) => {
+
+    axios.get(`https://itunes.apple.com/search?term=${parameter}&media=music`)
+      .then(data => {
+        setResults(data.data.results)
+      })
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.id
+    const value = event.target.value
+    setInput((...values) => ({ ...values, [name]: value }))
+    fetchData(value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        fetchData(input)
+      }}
+      >
+        <input
+          id="parameter"
+          type="text"
+          onChange={handleChange}
+          placeholder="Search"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </input>
+        <button
+          type='submit'
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Submit
+        </button>
+      </form>
+      {results.length > 0 && results.map(song => 
+      <div>
+        <p>{song.artistName}</p>
+        <p>{song.trackName}</p>
+        </div>)}
+    </section>
   );
 }
 
